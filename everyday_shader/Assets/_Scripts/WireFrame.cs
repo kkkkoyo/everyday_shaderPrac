fef92dfcd6949d7d791ab9bc31758a90852451ec
kkkkoyo <mmkrz.sg@gmail.com>
2018-04-23 01:13:32 +0900

    added unityPac , and wireframe

---
+++ everyday_shader/Assets/_Scripts/WireFrame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireFrame : MonoBehaviour {
+
+	// Use this for initialization
+    //参考: http://nn-hokuson.hatenablog.com/entry/2018/03/01/193935
+	void Start () {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), MeshTopology.Lines, 0);
+    }
+}
