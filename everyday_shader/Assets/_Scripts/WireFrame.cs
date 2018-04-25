c955848150d2ff83758b71c8eb6ea60e113e0272
kkkkoyo <mmkrz.sg@gmail.com>
2018-04-25 22:56:39 +0900

    changed wire

--- everyday_shader/Assets/_Scripts/WireFrame.cs
+++ everyday_shader/Assets/_Scripts/WireFrame.cs
@@ -6,8 +6,10 @@ public class WireFrame : MonoBehaviour {
 
 	// Use this for initialization
     //参考: http://nn-hokuson.hatenablog.com/entry/2018/03/01/193935
+    public Mesh mesh;
 	void Start () {
-        MeshFilter meshFilter = GetComponent<MeshFilter>();
-        meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), MeshTopology.Lines, 0);
+        SkinnedMeshRenderer meshFilter = GetComponent<SkinnedMeshRenderer>();
+        //meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), MeshTopology.Lines, 0);
+        mesh.SetIndices(mesh.GetIndices(0), MeshTopology.Lines, 0);
     }
 }
