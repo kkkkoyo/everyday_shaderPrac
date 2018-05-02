efbaa99aa3093b15d05a9949aab5b800dcda2007
kkkkoyo <mmkrz.sg@gmail.com>
2018-05-02 23:41:15 +0900

    いろいろついか

---
+++ everyday_shader/Assets/NewBehaviourScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	public Material material;
+	float intime = 1;
+	float outtime = 6.5f;
+	float size = 0.2f;
+	float size_time = 4;
+
+	void Start () {
+		// ２秒後にFadeIn()を、５秒後にFadeOut()を呼び出す
+		Invoke("FadeIn", intime);
+		Invoke("FadeOut", outtime);
+		StartCoroutine(Set());
+	}
+	IEnumerator Set(){
+		
+		while(true){
+		yield return new WaitForSeconds(outtime+size_time);
+		Invoke("FadeIn", intime);
+		Invoke("FadeOut", outtime);
+		}			
+
+	}
+	void FadeIn() {
+		// SetValue()を毎フレーム呼び出して、１秒間に０から１までの値の中間値を渡す
+		iTween.ValueTo(gameObject, iTween.Hash("from", 0f, "to", size, "time", size_time, "onupdate", "SetValue","EaseType", iTween.EaseType.easeInExpo));
+	}
+	void FadeOut() {
+		// SetValue()を毎フレーム呼び出して、１秒間に１から０までの値の中間値を渡す
+		iTween.ValueTo(gameObject, iTween.Hash("from", size, "to", 0f, "time", size_time, "onupdate", "SetValue","EaseType", iTween.EaseType.easeInOutQuart));
+	}
+	void SetValue(float alpha) {
+		// iTweenで呼ばれたら、受け取った値をImageのアルファ値にセット
+		material.SetFloat("_Displacement", alpha);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
--- everyday_shader/Assets/_Scripts/PointController.cs
+++ everyday_shader/Assets/_Scripts/PointController.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 public class PointController : MonoBehaviour {
 
 	// Use this for initialization
+	public bool isTruen;
 	void Start () {
-		MeshFilter meshFilter = GetComponent<MeshFilter>();
-        meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0),MeshTopology.Lines,0);
+
+        if (GetComponent<SkinnedMeshRenderer>())
+        {
+            SkinnedMeshRenderer smr = GetComponent<SkinnedMeshRenderer>();
+            smr.sharedMesh.SetIndices(smr.sharedMesh.GetIndices(0), MeshTopology.Lines, 0);
+        }
+        else if(GetComponent<MeshFilter>())
+        {
+            MeshFilter meshFilter = GetComponent<MeshFilter>();
+            meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), MeshTopology.Lines, 0);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
+		if(!isTruen){
+			transform.Rotate(new Vector3(0, 1, 0), 1);
+		}else{
+			transform.position = new Vector3(transform.position.x-0.001f,transform.position.y,transform.position.z);
+
+		}
 	}
 }
